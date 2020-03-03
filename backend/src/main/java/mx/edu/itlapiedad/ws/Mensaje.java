069c0aa139833113f953f3bf5a6c0e46fe122181
Chino <Chino@MSI>
2020-03-03 08:59:21 -0600

    Agregue un servicio y sha, otra vez.

--- backend/src/main/java/mx/edu/itlapiedad/ws/Mensaje.java
+++ backend/src/main/java/mx/edu/itlapiedad/ws/Mensaje.java
@@ -62,4 +62,13 @@ public class Mensaje {
 	}
 	//ArribalaMaquina
 	
+
+
+	// #ArribaLasPoderosas
+@GetMapping("calculadora/{num1}/{num2}")
+	public int calculadoraOp(@PathVariable int num1, @PathVariable int num2) {
+		int total= num1 + num2; 
+		return total;
+	}
+	
 }
